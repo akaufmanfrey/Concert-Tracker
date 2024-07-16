7eeb893cdbd73d94294982a1cca8f21606a55d80
unknown <chamberbankes@gmail.com>
2024-07-16 12:02:47 -0400

    hide load more main

--- js/script.js
+++ js/script.js
@@ -5,18 +5,19 @@ const concertCards = $('#concert-container');
 const loadMoreButton = $('#load-more-btn');
 
 function readArtistsFromStorage() {
-  
+
     //Retrieve artists from localStorage and parse the JSON to an array. If there are no artists in localStorage, initialize an empty array and return it.
     const storageArray = JSON.parse(localStorage.getItem("artists"));
     if (storageArray) {
-      return storageArray
+        return storageArray
     } else {
-      const emptyArray = [];
-      return emptyArray
+        const emptyArray = [];
+        return emptyArray
     }
-  
+
 }
 function displaySearchHistory() {
+    localStorage.setItem('next', '');
     const artistHistory = readArtistsFromStorage();
     if (artistHistory) {
         artistHistory.forEach(generateHistoryButton);
@@ -32,24 +33,25 @@ function generateHistoryButton(artist) {
 function getConcertResults(event) {
     event.preventDefault();
     const artistSearch = searchInput.val();
-    const apiUrl = 'https://api.predicthq.com/v1/events?q='+ artistSearch.replace(/\s/g, "+");
+    const apiUrl = 'https://api.predicthq.com/v1/events?q=' + artistSearch.replace(/\s/g, "+");
     console.log(apiUrl)
-fetch(apiUrl, {
-    headers: {
-        Authorization: "Bearer aZ6E2Dg5S1F-jxl_3A56LnvtDQEEqBw7rPP_5qgB",
-    },
-})
-  .then(function(response) {
-        if (response.ok) {
-            const artistArray = readArtistsFromStorage();
-            if (!(artistArray.includes(artistSearch))) {
-                artistArray.push(artistSearch);
-                generateHistoryButton(artistSearch);
-                localStorage.setItem('artists', JSON.stringify(artistArray));
-            }
+    fetch(apiUrl, {
+        headers: {
+            Authorization: "Bearer aZ6E2Dg5S1F-jxl_3A56LnvtDQEEqBw7rPP_5qgB",
+        },
+    })
+        .then(function (response) {
+            if (response.ok) {
+                const artistArray = readArtistsFromStorage();
+                if (!(artistArray.includes(artistSearch))) {
+                    artistArray.push(artistSearch);
+                    generateHistoryButton(artistSearch);
+                    localStorage.setItem('artists', JSON.stringify(artistArray));
+                }
             searchInput.val('');
             console.log(response);
             response.json().then(function(data) {
+                localStorage.setItem('next', data.next);
                 console.log(data.next);
                 concertCards.empty();
                 data.results.forEach(displayCard);
@@ -61,21 +63,41 @@ $('#load-more-btn').show();
 $('#load-more-btn').hide();
 }
         })
-        }
-    })
+}
+
+function loadMoreResults(event) {
+    event.preventDefault();
+    const apiUrl = localStorage.getItem('next');
+    if (apiUrl) {
+        fetch(apiUrl, {
+            headers: {
+                Authorization: "Bearer aZ6E2Dg5S1F-jxl_3A56LnvtDQEEqBw7rPP_5qgB",
+            },
+        })
+        .then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    localStorage.setItem('next', data.next);
+                    console.log(data.next);
+                    data.results.forEach(displayCard);
+                })
+            }
+        })
+    }
 }
 
 function displayCard(results) {
     console.log(results);
     const mainCard = $('<div>');
-    mainCard.addClass('block max-w-sm p-6 bg-gray border border-gray-300 rounded-lg shadow-md hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 card');
+    mainCard.addClass('block max-w-sm p-6 border border-gray-300 rounded-lg shadow-md card');
     const mainCardContent = $('<div>');
     mainCardContent.addClass('card-content')
     const cardTitle = $('<p>');
-    cardTitle.addClass('mb-2 text-3xl font-bold tracking-tight text-white'); 
+    cardTitle.addClass('mb-2 text-3xl font-bold tracking-tight text-white');
     cardTitle.text(results.title)
     const cardDate = $('<div>');
-    cardDate.addClass('mb-2 text-lg font-bold tracking-tight text-white'); 
+    cardDate.addClass('mb-2 text-lg font-bold tracking-tight text-white');
     cardDate.text(results.start_local);
     const cardDesc = $('<p>');
     cardDesc.addClass('mb-2 text-lg font-normal text-white dark:white');
@@ -92,25 +114,28 @@ function displayCard(results) {
 }
 
 searchButton.on('click', getConcertResults);
+loadMoreButton.on('click', loadMoreResults);
 $(document).ready(displaySearchHistory);
-$('aside').on('click', '.search-history', function(event) {
+$('aside').on('click', '.search-history', function (event) {
     searchInput.val($(event.target).text());
     searchButton.click();
 })
 
+
+
 //Dark Mode for index.html
 // script.js
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const darkModeToggle = document.getElementById("darkModeToggle");
-    
+
     // Check for saved user preference
     if (localStorage.getItem("dark-mode") === "enabled") {
         document.body.classList.add("dark");
     }
 
-    darkModeToggle.addEventListener("click", function() {
+    darkModeToggle.addEventListener("click", function () {
         document.body.classList.toggle("dark");
-        
+
         // Save user preference
         if (document.body.classList.contains("dark")) {
             localStorage.setItem("dark-mode", "enabled");
@@ -119,5 +144,4 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
 
